29c679de1fb77bf55797889401938c060cbda8a8
Dennis <den369e@edu.mercantec.dk>
2024-05-03 15:32:12 +0200

    feat: show message author profile picture

--- resources/views/components/chat/sender.blade.php
+++ resources/views/components/chat/sender.blade.php
@@ -11,9 +11,9 @@
             </button>
         </div>
 
-        <img class="h-14 w-14 rounded-full bg-gray-50"
-             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-             alt="">
+        <img class="h-14 w-14 rounded-full"
+             src="{{ asset($message->sender->profile_photo) }}"
+             alt="{{ $message->sender->username ?? 'Unknown' }}">
 
         <div class="relative flex flex-col h-full justify-content-start pl-4 text-white">
             <div class="font-bold text-lg">

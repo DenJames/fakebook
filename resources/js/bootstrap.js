32ba2b7a0a7db2b0f329d1ab260beead648dd308
Oliver Mogensen <k7h@fmshosting.net>
2024-05-21 15:25:14 +0200

    feat: Added install button

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,3 +32,27 @@ if ('serviceWorker' in navigator) {
         });
 }
 
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    showAddToHomeScreenButton();
+});
+
+function showAddToHomeScreenButton() {
+    const addToHomeScreenButton = document.getElementById('add-to-home-screen');
+    addToHomeScreenButton.style.display = 'block';
+
+    addToHomeScreenButton.addEventListener('click', () => {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+}
